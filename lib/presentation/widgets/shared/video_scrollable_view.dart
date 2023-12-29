12a315bf1206ea6ef245afb699ce45bd90f2ddc6
Luis Felipe Garcia <fellgar246@gmail.com>
2023-12-29 16:55:16 -0600

    add fullscreen player

--- lib/presentation/widgets/shared/video_scrollable_view.dart
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
 import 'package:toktik/presentation/widgets/shared/video_buttons.dart';
+import 'package:toktik/presentation/widgets/video/fullscreen_player.dart';
 
 class VideoScrollableView extends StatelessWidget {
   final List<VideoPost> videos;
@@ -22,7 +23,11 @@ class VideoScrollableView extends StatelessWidget {
           return Stack(
             children: [
               // Video Player + gradient
-
+              SizedBox.expand(
+                  child: FullScreenPlayer(
+                caption: videoPost.caption,
+                videoUrl: videoPost.videoUrl,
+              )),
               // Botones
               Positioned(
                   bottom: 40, right: 20, child: VideoButtons(video: videoPost))
